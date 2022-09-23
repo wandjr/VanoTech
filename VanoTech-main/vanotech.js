47f13ee19899bc8b6e7d66800cd718bda887cb5e
Rafael2207 <110902660+Rafael2207@users.noreply.github.com>
2022-09-23 07:49:08 -0300

    Add files via upload

---
+++ VanoTech-main/vanotech.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides;
+
+  for (i = 1; i < 6; i++) {
+    $("#carrossel"+i).hide(); 
+  }
+  slideIndex++;
+  if (slideIndex > 5) {slideIndex = 1}    
+
+  $("#carrossel"+slideIndex).show(4000);  
+
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
