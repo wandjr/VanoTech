32d81b1454fdaeeefde67234a3fe3436d972e886
bruno987-student <bruno_weber1@estudante.sc.senai.br>
2022-10-05 11:56:30 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -6,25 +6,15 @@
     $nome = $_POST["nome"];
     $sobrenome = $_POST["sobrenome"];
     $senha = MD5 ($_POST["senha"]);
-    $rua = $_POST["rua"];
-    $numero = $_POST["numero"];
     $telefone = $_POST["telefone"];
-    $bairro = $_POST["bairro"];
     $cpf = $_POST["cpf"];
-    $cidade = $_POST["cidade"];
-    $estado = $_POST["estado"];
     $data_nascimento = $_POST["data_nascimento"];
 
-    $comando = $pdo -> prepare("INSERT INTO usuario(email,nome,sobrenome,senha,estado,cidade,bairro,rua,numero,telefone,cpf,data_nascimento) VALUES(:email,:nome,:sobrenome,:senha,:estado,:cidade,:bairro,:rua,:numero,:telefone,:cpf,:data_nascimento)");
+    $comando = $pdo -> prepare("INSERT INTO usuario(email,nome,sobrenome,senha,telefone,cpf,data_nascimento) VALUES(:email,:nome,:sobrenome,:senha,:telefone,:cpf,:data_nascimento)");
     $comando->bindValue(":email", $email);
     $comando->bindValue(":nome", $nome);
     $comando->bindValue(":sobrenome", $sobrenome);
     $comando->bindValue(":senha", $senha);
-    $comando->bindValue(":estado", $estado);
-    $comando->bindValue(":cidade", $cidade);
-    $comando->bindValue(":bairro", $bairro);
-    $comando->bindValue(":rua", $rua);
-    $comando->bindValue(":numero", $numero);
     $comando->bindValue(":telefone", $telefone);
     $comando->bindValue(":cpf", $cpf);
     $comando->bindValue(":data_nascimento", $data_nascimento);
