6dc72dc9504a780ba5789e8203e6601cf9984836
bruno987-student <bruno_weber1@estudante.sc.senai.br>
2022-10-20 07:32:12 -0300

    klllhjhjjhkhj

--- servicos.php
+++ servicos.php
@@ -52,9 +52,11 @@ if(isset($_SESSION["loggedin"]))
         </div>
     </div>
 
-    <div id="recursos_humanos">
-        <div class="titulo_recursos">Recursos Humanos</div>
-        <img src="imagem/img2.png">
+    <div class="servicos">
+        <div class="titulo_imagem">        
+            <div class="titulo_recursos">Recursos Humanos</div>
+            <img src="imagem/img2.png">
+        </div>
         <p class="paragrafos1">
             A área de Recursos Humanos é a junção de colaboradores de uma determinada empresa. A base do segmento
             responsável são as ações de seleção, recrutamento, treinamento, remuneração e fornecimento de vantagens aos
@@ -62,27 +64,52 @@ if(isset($_SESSION["loggedin"]))
         </p>
     </div>
 
-    <div>Contabilidade</div>
-    <img src="imagem/img3.png">
-    <div>Contabilidade é a ciência que estuda e pratica as funções de orientação, controle e registro relativo aos atos
-        e fatos da administração econômica segundo o Congresso Brasileiro de Contabilidade.</div>
+    <div class="servicos">
+        <div class="titulo_imagem">        
+            <div class="titulo_recursos">Contabilidade</div>
+            <img src="imagem/img3.png">
+        </div>
+        <p class="paragrafos1">
+            Contabilidade é a ciência que estuda e pratica as funções de orientação, controle e registro relativo aos atos
+            e fatos da administração econômica segundo o Congresso Brasileiro de Contabilidade.
+        </p>
+    </div>
 
-    <div>Fiscal (Tax)</div>
-    <img src="imagem/img4.png">
-    <div>Acompanha as alterações nas legislações fiscais e tributária no âmbito Federal, Estadual e Municipal, realiza o
-        calculo de tributos, preenche e envia as obrigações acessórias.</div>
+    <div class="servicos">
+        <div class="titulo_imagem">        
+            <div class="titulo_recursos">Fiscal (Tax)</div>
+            <img src="imagem/img4.png">
+        </div>
+        <p class="paragrafos1">
+            Acompanha as alterações nas legislações fiscais e tributária no âmbito Federal, Estadual e Municipal, realiza o
+            calculo de tributos, preenche e envia as obrigações acessórias.
+        </p>
+    </div>
 
-    <div>Direito Societário</div>
-    <img src="imagem/img5.png">
-    <div>O Direito Societário compreende o estudo das sociedades, regras sobre a constituição e dissolução destas
-        sociedades, bem como os aspectos de relacionamentos entre elas e seus sócios e acionistas com às suas diversas
-        peculiaridades, como alterações de controle e de participação, direito de retirada etc.</div>
+    <div class="servicos">
+        <div class="titulo_imagem">        
+            <div class="titulo_recursos">Direito Societário</div>
+            <img src="imagem/img5.png">
+        </div>
+        <p class="paragrafos1">
+            O Direito Societário compreende o estudo das sociedades, regras sobre a constituição e dissolução destas
+            sociedades, bem como os aspectos de relacionamentos entre elas e seus sócios e acionistas com às suas diversas
+            peculiaridades, como alterações de controle e de participação, direito de retirada etc.
+        </p>
+    </div>
 
-    <div>Assessoria e Consultoria Empresarial</div>
-    <img src="imagem/img5.png">
-    <div>É uma prática empresarial que visa buscar o auxílio de profissionais externos à empresa para que ajudem a
-        organização a identificar fraquezas e definir planos de ação para superá-las e levar o negócio a atingir seus
-        objetivos estratégicos.</div>
+    <div class="servicos">
+        <div class="titulo_imagem">        
+            <div class="titulo_recursos">Assessoria e Consultoria Empresarial</div>
+            <br>
+            <img src="imagem/img6.png">
+        </div>
+        <p class="paragrafos1">
+            É uma prática empresarial que visa buscar o auxílio de profissionais externos à empresa para que ajudem a
+            organização a identificar fraquezas e definir planos de ação para superá-las e levar o negócio a atingir seus
+            objetivos estratégicos.
+        </p>
+    </div>
 
     <a href="contratar_servico.php">
         <button class="botao_contrato">
