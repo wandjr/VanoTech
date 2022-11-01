655e271892f8d163b1614368e39ec42b15c675ff
wandjr <110992185+wandjr@users.noreply.github.com>
2022-11-01 07:45:09 -0300

    Update contratar_servico.php

--- contratar_servico.php
+++ contratar_servico.php
@@ -1,21 +1,90 @@
-<?php
-    session_start();
-    // Verifique se o usuário está logado, se não, redirecione-o para uma página de login
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true){
-        header("location: cadastro.php");
-        exit;
-    }
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible">
+    <title>CADASTRO</title>
+    <link rel="stylesheet" href="vanotech.css" />
+    <link href='https://fonts.googleapis.com/css?family=ABeeZee' rel='stylesheet'>
 </head>
 <body>
-    mkhjghjghjede
+    <div class="menu">
+            <img src="imagem/logo.png" id="logo" width="70px" height="70px">
+            <a href="home.php"><button class="botao">HOME</button></a>
+            <a href="servicos.php"><button class="botao">SERVIÇOS</button></a>
+            <a href="trabalhe_conosco.php"><button class="botao">TRABALHE CONOSCO</button></a>
+            <a href="simulacao_de_Contrato.php"><button class="botao">SIMULAÇÃO DE CONTRATO</button></a>
+            <a href="contato.php"><button class="botao">CONTATO</button></a>
+            <a href="perfil.php"> <button class="botao">PERFIL</button></a>
+    </div>
+
+    <div class="conteudo">
+        <div class="texto">
+            <p id="paragrafos_servico">
+                Faça simulações e contratações de serviços com sua conta. É simples e rápido:
+            </p>
+        </div>
+    </div>
+
+    <form action="inserir.php" method="post">
+        <div class="cadastro">
+            E-mail
+            <input type="email" id="email" name="email" class="caixa_texto" placeholder="pedro@gmail.com">
+        </div>
+        <br>
+
+
+        <div class="cadastro">
+            Nome
+            <input type="text" name="nome" class="caixa_texto" placeholder="Mirianda">
+        </div>
+        <br>
+
+        <div class="cadastro">
+                Sobrenome
+                <input type="text" name="sobrenome" class="caixa_texto" placeholder="Souza">
+            </div>
+            <br>
+
+        <div class="cadastro">
+            Senha
+            <input type="password" name="senha" class="caixa_texto" placeholder="Senha123">
+        </div>
+        <br>
+
+        <div class="cadastro">
+            Confirmar Senha
+            <input type="password" name="senha" class="caixa_texto" placeholder="Senha123">
+        </div>
+        <br>
+
+        <div class="cadastro">
+            Telefone
+            <input type="number" name="telefone" class="caixa_texto" placeholder="(47) 99593-9247">
+        </div>
+        <br>
+
+        <div class="cadastro">
+            CPF
+            <input type="text" name="cpf" class="caixa_texto" placeholder="123.456.789-10">
+        </div>
+        <br>
+
+        <div class="cadastro">
+            Data de Nascimento
+            <input type="date" name="data_nascimento" class="caixa_texto" placeholder="22/06/1992">
+        </div>
+        <br>
+
+        <a href="login.html"><button onmousemove="validar();" class="botao_cadastro">Fazer Cadastro</button></a>
+        <br>
+    </form>
+
+    <div id="texto">Já possui cadastro?<a href="login.html" >Login</a></div>
+    
+    <br>
 </body>
+
+
 </html>
